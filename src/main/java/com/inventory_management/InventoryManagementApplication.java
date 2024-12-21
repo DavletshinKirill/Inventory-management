d63b899fea1147d194f08dae8d86dfc254d521f0
kirill <kirill.davletshin.02@yandex.ru>
2024-12-21 17:36:07 +0300

    dockerize app

--- src/main/java/com/inventory_management/InventoryManagementApplication.java
+++ src/main/java/com/inventory_management/InventoryManagementApplication.java
@@ -1,13 +1,31 @@
 package com.inventory_management;
 
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class InventoryManagementApplication {
+@Slf4j
+public class InventoryManagementApplication implements CommandLineRunner {
 
+    @Value("${spring.datasource.url}")
+    private String dbUrl;
+    @Value("${spring.security.oauth2.resourceserver.jwt.issuer-uri}")
+    private String issuerUri;
+
+    @Value("${spring.security.oauth2.resourceserver.jwt.jwk-set-uri}")
+    private String jwkSetUri;
     public static void main(String[] args) {
         SpringApplication.run(InventoryManagementApplication.class, args);
     }
 
+    @Override
+    public void run(String... args) throws Exception {
+        log.info("Run method begging");
+        log.info("Issuer URI: {}", issuerUri);
+        log.info("JWK Set: {}", jwkSetUri);
+        log.info("dbUrl Set: {}", dbUrl);
+    }
 }
